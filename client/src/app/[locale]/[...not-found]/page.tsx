c5bcca2b42014453c5863e088fde70206313e341
GEO\lieu.hong.thai <lieu.hong.thai@geonet.co.jp>
2024-01-22 11:13:33 +0700

    [Update] update not found page

--- client/src/app/[locale]/[...not-found]/page.tsx
+++ client/src/app/[locale]/[...not-found]/page.tsx
@@ -2,10 +2,8 @@ import { LayoutTypes } from '@/@core/types/app-routes/type';
 import { notFound } from 'next/navigation';
 import React from 'react';
 
-type Props = {};
-
 const RestPage = (props: LayoutTypes) => {
-  console.log(12005, 'RestPage: ', props.params.locale);
+  console.log(12005, 'RestPage: ', props);
 
   return notFound();
 };
--- client/src/app/[locale]/not-found.tsx
+++ client/src/app/[locale]/not-found.tsx
@@ -1,46 +1,26 @@
 'use client';
-
-// ** React Imports
-import { ReactNode } from 'react';
-
 // ** Next Import
 import Link from 'next/link';
 
 // ** MUI Components
 import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import Box, { BoxProps } from '@mui/material/Box';
+import Box from '@mui/material/Box';
 import BlankLayout from '@/@core/layouts/BlankLayout';
+import Image from 'next/image';
 
-// ** Styled Components
-const BoxWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  [theme.breakpoints.down('md')]: {
-    width: '90vw',
-  },
-}));
+import NotFoundImage from '../../../public/images/pages/404.png';
 
-const Img = styled('img')(({ theme }) => ({
-  marginTop: theme.spacing(15),
-  marginBottom: theme.spacing(15),
-  [theme.breakpoints.down('lg')]: {
-    height: 450,
-    marginTop: theme.spacing(10),
-    marginBottom: theme.spacing(10),
-  },
-  [theme.breakpoints.down('md')]: {
-    height: 400,
-  },
-}));
-
-export default function NotFound(props: any) {
-  console.log(12005, 'NotFound', props);
-
+export default function NotFound() {
   return (
     <BlankLayout>
       <Box className='content-center'>
         <Box sx={{ p: 5, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-          <BoxWrapper>
+          <Box
+            sx={{
+              width: theme => theme.breakpoints.down('sm') && '90vw',
+            }}
+          >
             <Typography variant='h1' sx={{ mb: 2.5 }}>
               404
             </Typography>
@@ -48,8 +28,9 @@ export default function NotFound(props: any) {
               Page Not Found ⚠️
             </Typography>
             <Typography variant='body2'>We couldn&prime;t find the page you are looking for.</Typography>
-          </BoxWrapper>
-          <Img alt='error-illustration' src='/images/pages/404.png' />
+          </Box>
+          <Image src={NotFoundImage} alt='error-illustration' />
+
           <Button href='/' component={Link} variant='contained' sx={{ px: 5.5 }}>
             Back to Home
           </Button>
